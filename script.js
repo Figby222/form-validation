114cc9cc164a61da90aeb88a6c0283b58f3b8660
Figby222 <figby222@outlook.com>
2024-03-09 05:22:12 -0800

    Return 1 on error functions if input is invalid

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const userPassword = document.querySelector('#password');
 const userConfirmPassword = document.querySelector('#confirm-password');
 const zipRegExp = /\d{5}/g
 
-function showError() {
-    if (!(userEmail.checkValidity())) {
-        userEmail.setCustomValidity("Please enter a valid email address")
-    }
+function checkError() {
+    
 
 }
 
 function checkEmailError() {
     if (userEmail.validity.typeMismatch) {
         userEmail.setCustomValidity("Please enter a valid email address")
+        return 1;
     } else {
         userEmail.setCustomValidity("");
+        return 0;
     }
     // userEmail.reportValidity();
 }
@@ -24,8 +24,10 @@ function checkEmailError() {
 function checkCountryError() {
     if (userCountry.value == "placeholder") {
         userCountry.setCustomValidity("Please select a country");
+        return 1;
     } else {
         userCountry.setCustomValidity("");
+        return 0;
     }
     // userCountry.reportValidity();
 }
@@ -33,8 +35,10 @@ function checkCountryError() {
 function checkZipError() {
     if (!(zipRegExp.test(userZip.value))) {
         userZip.setCustomValidity("Please enter a valid ZIP code");
+        return 1;
     } else {
         userZip.setCustomValidity("")
+        return 0;
     }
     // userZip.reportValidity();
 }
@@ -43,9 +47,11 @@ function checkPasswordError() {
     if (userPassword.value != userConfirmPassword.value) {
         userPassword.setCustomValidity("Passwords must match");
         userConfirmPassword.setCustomValidity("Passwords must match");
+        return 1;
     } else {
         userPassword.setCustomValidity("");
         userConfirmPassword.setCustomValidity("");
+        return 0;
     }
 
     // userPassword.reportValidity();
